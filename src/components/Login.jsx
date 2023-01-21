0e22fba3288076254b14acbda9c37aa32047d032
shukeel <triggaillest@gmail.com>
2023-01-21 15:47:55 +0300

    modified login & register components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from '../api/axios';
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('/login', {email, password});
+      
+      setEmail("")
+      setPassword("")
+      navigate('/');
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
   return (
     <>
         <div>
             <h3>Login</h3>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
         <div className="mb-6">
             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input 
             type="email" 
+            value={email}
+            onChange={(e) => {setEmail(e.target.value)}}
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             placeholder="name@flowbite.com" 
@@ -24,6 +49,8 @@ function Login() {
             <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
             <input 
             type="password"
+            value={password}
+            onChange={(e) => {setPassword(e.target.value)}}
             id="password" 
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             required />
@@ -35,7 +62,7 @@ function Login() {
          >
          Forgot password?
         </Link>
-               <Link 
+        <Link 
          to='/register'
          className='mt-2 inline-block hover:text-primary hover:underline'
          >
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from '../api/axios';
 
 function Register() {
+  const [name, setName] = useState("");    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('/register', {name, email, password, password_confirmation});
+      
+      setName("")
+      setEmail("")
+      setPassword("")
+      setPasswordConfirmation("")
+      navigate('/');
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
         <>
         <div>
             <h3>Register</h3>
         </div>
-        <form>
+        <form onSubmit={handleRegister}>
         <div className="mb-6">
             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full name</label>
             <input 
-            type="email" 
-            id="email"
+            type="name" 
+            value={name}
+            onChange={(e) => {setName(e.target.value)}}
+            id="name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             placeholder="John Doe" 
             required />
@@ -20,6 +48,8 @@ function Register() {
             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input 
             type="email" 
+            value={email}
+            onChange={(e) => {setEmail(e.target.value)}}
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             placeholder="name@flowbite.com" 
@@ -29,20 +59,30 @@ function Register() {
             <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
             <input 
             type="password"
+               value={password}
+            onChange={(e) => {setPassword(e.target.value)}}
             id="password" 
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             required />
         </div>
                 <div className="mb-6">
-            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+            <label for="password_confirmation" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
             <input 
             type="password"
-            id="password" 
+               value={password_confirmation}
+            onChange={(e) => {setPasswordConfirmation(e.target.value)}}
+            id="password_confirmation" 
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             required />
         </div>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
         </form>
+         <Link 
+         to='/login'
+         className='mt-2 inline-block hover:text-primary hover:underline'
+         >
+         sign in
+        </Link>
     </>
   )
 }
